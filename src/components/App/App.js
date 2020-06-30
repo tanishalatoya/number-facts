578fe2f272605ce258956734891555570ab6c83e
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-06-30 15:29:19 -0600

    Render FactContainer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SearchForm from '../SearchForm/SearchForm';
+import FactContainer from '../FactContainer/FactContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <SearchForm />
+      <FactContainer />
     </div>
   );
 }
