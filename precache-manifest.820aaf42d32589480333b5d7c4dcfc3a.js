71fff30d7eda0f2ce6f983f054a05d928508b949
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-06-30 15:04:14 -0600

    Updates

---
+++ precache-manifest.820aaf42d32589480333b5d7c4dcfc3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50ff5df6c7bfe04567701ab71b3011a",
+    "url": "/number-facts/index.html"
+  },
+  {
+    "revision": "02dba4db4057d75db9b2",
+    "url": "/number-facts/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb951778dcc35a4ae9e4",
+    "url": "/number-facts/static/js/2.5dc6fb33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/number-facts/static/js/2.5dc6fb33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02dba4db4057d75db9b2",
+    "url": "/number-facts/static/js/main.2f0560fe.chunk.js"
+  },
+  {
+    "revision": "76eb986817a3358b3b89",
+    "url": "/number-facts/static/js/runtime-main.6b28ea03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/number-facts/static/media/logo.5d5d9eef.svg"
+  }
+]);
